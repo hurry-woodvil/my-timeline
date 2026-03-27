18ab00b245244ee8d3bf5eabb5fca2a48cd2723a
hurry-woodvil <hurry.woodvil@gmail.com>
2026-03-27 10:18:07 +0900

    feat: add select method to repository (#57)

--- backend/src/common/repository/memories.rs
+++ backend/src/common/repository/memories.rs
@@ -2,12 +2,12 @@ use std::{collections::HashMap, sync::Arc};
 
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
-use sqlx::SqlitePool;
+use sqlx::{FromRow, SqlitePool};
 use tokio::sync::RwLock;
 
 use crate::common::error::AppError;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, FromRow)]
 pub struct Memory {
     pub memory_id: String,
     pub user_id: String,
@@ -18,6 +18,11 @@ pub struct Memory {
 
 #[async_trait]
 pub trait MemoriesRepositoryTrait: Send + Sync {
+    async fn select_by_user_id(
+        &self,
+        db: &SqlitePool,
+        user_id: &str,
+    ) -> Result<Vec<Memory>, AppError>;
     async fn insert_memory(&self, db: &SqlitePool, memory: &Memory) -> Result<(), AppError>;
 }
 
@@ -25,20 +30,28 @@ pub type MemoriesRepository = Arc<dyn MemoriesRepositoryTrait + Send + Sync>;
 
 pub struct InMemoryMemoriesRepository {
     pub memories_by_memory_id: RwLock<HashMap<String, Memory>>,
-    pub memories_by_user_id: RwLock<HashMap<String, Memory>>,
 }
 
 #[async_trait]
 impl MemoriesRepositoryTrait for InMemoryMemoriesRepository {
+    async fn select_by_user_id(
+        &self,
+        _db: &SqlitePool,
+        user_id: &str,
+    ) -> Result<Vec<Memory>, AppError> {
+        let map = self.memories_by_memory_id.read().await;
+
+        Ok(map
+            .values()
+            .filter(|m| m.user_id == user_id)
+            .cloned()
+            .collect())
+    }
+
     async fn insert_memory(&self, _db: &SqlitePool, memory: &Memory) -> Result<(), AppError> {
         {
             let mut map = self.memories_by_memory_id.write().await;
             map.insert(memory.memory_id.clone(), memory.clone());
-        }
-
-        {
-            let mut map = self.memories_by_user_id.write().await;
-            map.insert(memory.user_id.clone(), memory.clone());
         }
 
         Ok(())
@@ -49,6 +62,31 @@ pub struct InDatabaseMemoriesRepository;
 
 #[async_trait]
 impl MemoriesRepositoryTrait for InDatabaseMemoriesRepository {
+    async fn select_by_user_id(
+        &self,
+        db: &SqlitePool,
+        user_id: &str,
+    ) -> Result<Vec<Memory>, AppError> {
+        let memories = sqlx::query_as::<_, Memory>(
+            r#"
+            SELECT
+                memory_id,
+                user_id,
+                content,
+                created_at,
+                updated_at
+            FROM memories
+            WHERE user_id = ?
+            ORDER BY created_at DESC
+            "#,
+        )
+        .bind(user_id)
+        .fetch_all(db)
+        .await?;
+
+        Ok(memories)
+    }
+
     async fn insert_memory(&self, db: &SqlitePool, memory: &Memory) -> Result<(), AppError> {
         let result = sqlx::query(
             r#"
