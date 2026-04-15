672e944c05fae058f9cecf6996bce09055add4f9
hurry-woodvil <hurry.woodvil@gmail.com>
2026-04-15 15:10:39 +0900

    feat: add update endpoint (#102)

--- backend/src/routes/memory.rs
+++ backend/src/routes/memory.rs
@@ -1,6 +1,6 @@
 use axum::{
     Router, middleware,
-    routing::{delete, get, post},
+    routing::{delete, get, patch, post},
 };
 
 use crate::{
@@ -12,10 +12,12 @@ pub fn router(state: AppState) -> Router<AppState> {
 
     let fetch_path = format!("{}/{{memory_id}}", base_path);
     let delete_path = format!("{}/{{memory_id}}", base_path);
+    let update_path = format!("{}/{{memory_id}}", base_path);
 
     Router::new()
         .route(&fetch_path, get(handler::fetch_memory))
         .route(&base_path, post(handler::post_memory))
+        .route(&update_path, patch())
         .route(&delete_path, delete(handler::delete_memory))
         .route_layer(middleware::from_fn_with_state(state, require_auth))
 }
